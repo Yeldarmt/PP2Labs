ac33c6bdc659f4a3f4f7a9e67002859050e7e77b
Administrator <eldarmukhametkazin@gmail.com>
2018-01-17 10:02:28 +0600

    first week

---
+++ w1/Program1Primenumber_2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok = true;
+            string s = Console.ReadLine();/* стринг аламыз "1 2 3 4 5"*/
+            string[] arr = s.Split(' '); /* жеке сан ретинде болип аламыз {"1","2","3","4","5"} */
+            int[] array = new int[arr.Length]; /* жана массив аламыз*/
+            for (int i = 0; i < array.Length; i++) /* массив аркылы журемиз */
+            {
+                array[i] = int.Parse(arr[i]);   /* Массивтеги барлык элементтерди санга айналдырамыз */
+            }
+            foreach (int q in array) /* массивпен журемиз */
+            {
+                for (int j = 2; j <= Math.Sqrt(q); j++) /* жай санга тексеремиз */
+                {
+                    if (q % j == 0)
+                    {
+                        ok = false;
+                        break;
+                    }
+                    else
+                    {
+                        ok = true;
+                    }
+
+                }
+                if (ok && q != 1)
+                {
+                    Console.WriteLine(q);
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ w2/Project1/Project1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] arr = line.Split(' ');
+            
+            for(int i=0 ; i < arr.Length ; i++)
+            {
+                int k = 0;
+                for (int j = 2; j < int.Parse(arr[i]); j++)
+                {
+                    
+                    if ((int.Parse(arr[i])) % j == 0)
+                    {
+                        k++;
+                    }
+                }
+                if (k == 0)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            } 
+            Console.ReadKey();
+
+        }
+    }
+}
