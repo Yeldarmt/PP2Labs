17166224b864f73192d89384a9765061b57675e3
Administrator <eldarmukhametkazin@gmail.com>
2018-01-28 19:14:09 +0600

    123

---
+++ w1/Mcircle/Mcircle/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcircle
+{
+    class Circle
+    {
+        public double r;
+        public double s;
+        public double d;
+        public double l;
+
+        public Circle()
+        {
+            r = 5;
+            FindArea();
+            FindDiametr();
+            FindCircumference();
+        }
+
+        public Circle(double r)
+        {
+            this.r = r;
+            FindArea();
+            FindDiametr();
+            FindCircumference();
+
+        }
+        public void FindArea()
+        {
+            s = Math.PI * r * r;
+        }
+        public void FindDiametr()
+        {
+            d = 2 * r;
+        }
+        public void FindCircumference()
+        {
+            l = 2 * Math.PI * r;
+        }
+        public override string ToString()
+        {
+            return "Area = " + s + "\nDiametr = " + d + "\nCircumference = " + l;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius of circum");
+            double r = int.Parse(Console.ReadLine());
+            Circle c = new Circle(r);
+            Console.WriteLine(c);
+            Console.WriteLine("The Next Circle :");
+            Circle c2 = new Circle();
+            Console.WriteLine(c2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ w1/Mrectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle
+{  
+    class rectangle
+    {
+        public double w;
+        public double h;
+        public double s;
+        public double p;
+        public rectangle(double w, double h)
+        {
+            this.w = w;
+            this.h = h;
+            FindArea();
+            FindPerimetr();
+        }
+        void FindArea()
+        {
+            this.s = w * h;
+            
+        }
+        void FindPerimetr()
+        {
+            this.p = 2 * (w + h);
+        }
+        public override string ToString()
+        {
+            return "Area = " + s + "\nPerimetr = " + p;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter width :");
+            double w = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height :");
+            double h = double.Parse(Console.ReadLine());
+            rectangle rec = new rectangle(w,h);
+            Console.WriteLine(rec);
+            Console.ReadKey();
+        }
+    }
+}
+
