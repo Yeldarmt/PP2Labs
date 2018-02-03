f5e467946e1e95343dbb809cab6fca736e451b00
Administrator <eldarmukhametkazin@gmail.com>
2018-02-03 16:18:59 +0600

    12345

---
+++ w4/snake1/snake/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int level = 1;
+            Snake snake = new Snake();
+            Wall wall = new Wall();
+            while(true){
+                try
+                {
+                    /*Console.Clear();
+                    Console.CursorVisible = false;
+                    Console.SetCursorPosition(x, y);
+                    Console.Write("*");*/
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    if (keyInfo.Key == ConsoleKey.UpArrow)
+                        snake.Move(0, -1);
+                    if (keyInfo.Key == ConsoleKey.DownArrow)
+                        snake.Move(0, 1);
+                    if (keyInfo.Key == ConsoleKey.LeftArrow)
+                        snake.Move(-1,0);
+                    if (keyInfo.Key == ConsoleKey.RightArrow)
+                        snake.Move(1, 0);
+                    
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("ERROR!");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                snake.Draw();
+                wall.Draw();
+            }
+        }
+    }
+}
---
+++ w4/snake1/snake/Wall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Wall
+    {
+        public List<Point> body;
+        public string sign;
+        public ConsoleColor color;
+
+        /*public void ReadLevel(int level)
+        {
+            StreamReader sr=new StreamReader(@"C:\Desktop")
+                int n = int.Parse(sr.Readline());
+            for (int i = 0; i < n; i++)
+            {
+                if(sr[j])
+            }
+        }*/
+        public void Draw()
+        {
+            Console.ForegroundColor = color;
+            foreach (Point p in body)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.Write(sign);
+            }
+        }
+    }
+}
